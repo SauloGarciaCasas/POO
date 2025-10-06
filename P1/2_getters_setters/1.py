08f5ea0201f62f5c64ccc69da4896b31ec0aeb91
SauloGarciaCasas <saulo_3141240224@utd.edu.mx>
2025-10-05 23:56:22 -0600

    Delete P1 directory

--- P1/2_getters_setters/1.py
+++
@@ -1,8 +0,0 @@
-def calculararea(base, altura):
-    calcular = (base * altura) / 2
-    return calcular
-
-base = float(input("Ingrese la base de un rectángulo: "))
-altura = float(input("Ingrese la altura de un rectángulo: "))
-total = calculararea(base, altura)
-print("El área es: ", total)
